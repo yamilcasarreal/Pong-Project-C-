e9e359feed512e80633af997aaa06adb52ca794a
Yamil Casarreal <yamilcasarreal@gmail.com>
2024-06-14 00:58:27 -0400

    Added ai and player movement

--- main.cpp
+++ main.cpp
@@ -1,12 +1,136 @@
-#include <iostream>
 #include <raylib.h>
+#include <iostream>
 
 using namespace std;
 
+class Ball
+{
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw()
+    {
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Update()
+    {
+        x += speed_x;
+        y += speed_y;
+
+        if (y + radius >= GetScreenHeight() || y - radius <= 0)
+        {
+            speed_y *= -1;
+        }
+        if (x + radius >= GetScreenWidth() || x - radius <= 0)
+        {
+            speed_x *= -1;
+        }
+    }
+};
+
+class Paddle
+{
+protected:
+    void LimitMovement()
+    {
+        if (y <= 0)
+            y = 0;
+        if (y + height >= GetScreenHeight())
+            y = GetScreenHeight() - height;
+    }
+
+public:
+    float x, y;
+    float width, height;
+    int speed;
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+
+    void Update()
+    {
+        if (IsKeyDown(KEY_UP))
+        {
+            y = y - speed;
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            y = y + speed;
+        }
+        LimitMovement();
+    }
+};
+
+class CpuPaddle : public Paddle
+{
+public:
+    void Update(int ball_y)
+    {
+        if (y + height / 2 > ball_y)
+        {
+            y -= speed;
+        }
+        else
+            y += speed;
+
+        LimitMovement();
+    }
+};
+Ball ball;
+Paddle player;
+CpuPaddle cpu;
 int main()
 {
 
-    cout << "Hello World" << endl;
+    // cout << "Hello World" << endl;
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "Pong");
+    SetTargetFPS(60);
 
+    // ball properities
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+    // player properties
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height / 2 - player.height / 2;
+    player.speed = 6;
+
+    // cpu properties
+    cpu.height = 120;
+    cpu.width = 25;
+    cpu.x = 10;
+    cpu.y = screen_height / 2 - cpu.height / 2;
+    cpu.speed = 6;
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+
+        // Updating
+        ball.Update();
+        player.Update();
+
+        cpu.Update(ball.y);
+
+        // Drawing
+        ClearBackground(BLACK);
+        ball.Draw();
+        player.Draw();
+        cpu.Draw();
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
